e5094cd048f95f5bb3af3ee9891541e24c26e3f7
John Peterson <johndpete5316@outlook.com>
2025-10-08 23:20:42 -0400

    file serving re-work
    This is a pivotal commit in this project becuase I am moving away from
    the psuedo router I had before and moving to serving static files. I've
    implimented path santitization, and a basic router with some MAJOR logic
    problems that I will work out later.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -22,8 +23,10 @@ import (
 
 // more info @ https://www.jmarshall.com/easy/http/
 
+// should be configurable by config file eventually
 const PORT string = ":80"
 const VERSION string = "HTTP/1.0"
+const DOCUMENTROOT string = "./public"
 
 type Request struct {
 	Method  string
@@ -50,7 +53,7 @@ var statusReasons = map[string]string{
 	"418": "I'm a teapot",
 }
 
-type requestHandler func(Request) Response
+// type requestHandler func(Request) Response
 
 func main() {
 	fmt.Printf("Listening on port %s\n", PORT)
@@ -71,6 +74,35 @@ func main() {
 
 }
 
+func sanitizePath(userPath string, documentRoot string) (string, error) {
+
+	// TODO: Re-arange this file so that path traversal attempts can be detected and rejected
+	// prior to the filepath.Clean function is ran, scumbags need a 403 xD
+
+	var safePath string
+
+	cleanedPath := filepath.Clean(userPath)
+	joinedPath := filepath.Join(documentRoot, cleanedPath)
+
+	absPath, err := filepath.Abs(joinedPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to get absolute path: %w", err)
+	}
+	absDocumentRoot, err := filepath.Abs(documentRoot)
+	if err != nil {
+		return "", fmt.Errorf("failed to get absolute path: %w", err)
+	}
+	if strings.HasPrefix(absPath, absDocumentRoot) {
+		// TODO: validate next char after prefix is a path seperator
+		safePath = absPath
+	} else {
+		return "", fmt.Errorf("path traversal attempt")
+	}
+	fmt.Printf("absolute path: %s\n", absPath)
+
+	return safePath, nil
+}
+
 func parseRequest(r *bufio.Reader) (*Request, error) {
 
 	req := Request{
@@ -84,7 +116,6 @@ func parseRequest(r *bufio.Reader) (*Request, error) {
 		if err != nil {
 			return nil, fmt.Errorf("failed to read the request line: %w", err)
 		}
-
 		if message == "\r\n" {
 			// read body
 			sbodyLen, ok := req.Headers["Content-Length"]
@@ -108,16 +139,12 @@ func parseRequest(r *bufio.Reader) (*Request, error) {
 			req.Body = string(body)
 			break
 		}
-
 		sMessage := strings.ReplaceAll(message, "\r\n", "")
 		if i == 0 {
-
 			s := strings.Fields(sMessage)
-
 			if len(s) < 3 {
 				return nil, fmt.Errorf("malformed request line")
 			}
-
 			req.Method = s[0]
 			req.Path = s[1]
 			req.Version = s[2]
@@ -127,12 +154,10 @@ func parseRequest(r *bufio.Reader) (*Request, error) {
 			if len(s) == 2 {
 				headerKey := s[0]
 				req.Headers[headerKey] = strings.TrimLeftFunc(s[1], unicode.IsSpace)
-
 			} else {
 				return nil, fmt.Errorf("malformed headers")
 			}
 		}
-
 		i++
 	}
 
@@ -140,13 +165,16 @@ func parseRequest(r *bufio.Reader) (*Request, error) {
 }
 
 func formatResponse(r Response) string {
+
 	var sb strings.Builder
+
 	sb.WriteString(fmt.Sprintf("%s %s %s\r\n", r.Version, r.ResCode, r.Reason))
 	for k, v := range r.Headers {
 		sb.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
 	}
 	sb.WriteString("\r\n")
 	sb.WriteString(r.Body)
+
 	return sb.String()
 }
 
@@ -155,7 +183,6 @@ func buildResponse(body string, statusCode string) Response {
 	if !ok {
 		reason = "UNKNOWN"
 	}
-
 	resp := Response{
 		Version: VERSION,
 		ResCode: statusCode,
@@ -167,7 +194,6 @@ func buildResponse(body string, statusCode string) Response {
 		},
 		Body: string(body),
 	}
-
 	return resp
 }
 
@@ -187,6 +213,9 @@ func serveFile(filename string, statusCode string) Response {
 
 func handleTCPConnections(c net.Conn) {
 	defer c.Close()
+
+	var isDir bool
+
 	// request
 	fmt.Printf("Connection started from %s\n", c.RemoteAddr().String())
 	r := bufio.NewReader(c)
@@ -201,33 +230,65 @@ func handleTCPConnections(c net.Conn) {
 		return
 	}
 
-	var routes = map[string]requestHandler{
-		"/": func(req Request) Response {
-			resp := serveFile("index.html", "200")
-			return resp
-		},
-		"/abcd": func(req Request) Response {
-			resp := serveFile("abcd.html", "200")
-			return resp
-		},
-		"/418": func(req Request) Response {
-			resp := serveFile("teapot.html", "418")
-			return resp
-		},
+	// TODO: MIME Type detection, adjust Content-Type header based off this.
+	// TODO: handle file not found  (404)
+	// TODO: handle DIR not found  (404)
+	// TODO: run through full test suite --
+	// GET / → should serve index.html
+	// GET /about.html → should serve about.html
+	// GET /images/cat.jpg → should serve the image with correct Content-Type
+	// GET /../../../etc/passwd → should return 403
+	// GET /nonexistent.html → should return 404
+	// TODO: Remove routes map comment block
+	// TODO: config file so I can bring back my dream of the teapot page returning 418
+
+	sanitizedPath, _ := sanitizePath(req.Path, DOCUMENTROOT)
+	fmt.Println(sanitizedPath)
+	fileStats, err := os.Stat(sanitizedPath)
+	if err != nil {
+		isDir = false
+	} else {
+		isDir = fileStats.IsDir()
 	}
-
-	if handler, exists := routes[req.Path]; exists {
-		resp := handler(*req)
+	if isDir {
+		resp := serveFile(sanitizedPath+"/index.html", "200")
 		res := formatResponse(resp)
-
 		c.Write([]byte(res))
 		return
 	} else {
-		resp := serveFile("not-found.html", "404")
+		resp := serveFile(sanitizedPath+".html", "200")
 		res := formatResponse(resp)
-
 		c.Write([]byte(res))
 		return
 	}
+
+	/* 	var routes = map[string]requestHandler{
+	   		"/": func(req Request) Response {
+	   			resp := serveFile("index.html", "200")
+	   			return resp
+	   		},
+	   		"/abcd": func(req Request) Response {
+	   			resp := serveFile("abcd.html", "200")
+	   			return resp
+	   		},
+	   		"/418": func(req Request) Response {
+	   			resp := serveFile("teapot.html", "418")
+	   			return resp
+	   		},
+	   	}
+
+	   	if handler, exists := routes[req.Path]; exists {
+	   		resp := handler(*req)
+	   		res := formatResponse(resp)
+
+	   		c.Write([]byte(res))
+	   		return
+	   	} else {
+	   		resp := serveFile("not-found.html", "404")
+	   		res := formatResponse(resp)
+
+	   		c.Write([]byte(res))
+	   		return
+	   	} */
 
 }
